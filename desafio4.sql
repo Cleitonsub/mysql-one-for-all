25fc744a7a59149c18d79f234461ee9d2625575b
Cleiton <cleitonrmacedo@gmail.com>
2022-04-17 20:41:44 -0300

    feat: Requisito 4 - Criada uma QUERY que mostra as pessoas usuarias que estavam ativas no ano de 2021 se baseando na data mais recente no historico de reproducao

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT usu.usuario,
+IF(YEAR(his.data_rep) >= 2021, 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+FROM SpotifyClone.usuarios AS usu
+LEFT JOIN SpotifyClone.historico_reps AS his
+ON usu.usuario_id = his.usuario_id
+AND YEAR(his.data_rep) >= 2021
+ORDER BY usu.usuario;
