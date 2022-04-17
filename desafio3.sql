eaeea9c54de81778f51c73956cf0a394c587477f
Cleiton <cleitonrmacedo@gmail.com>
2022-04-17 18:06:51 -0300

    fix: Requisito 3 - Deixando mais especifico de quais tabelas os dados devem ser pegos

--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,8 @@
 SELECT usu.usuario, COUNT(his.usuario_id) AS qtde_musicas_ouvidas,
 ROUND(SUM(can.duracao_seg) / 60, 2) AS total_minutos
-FROM usuarios AS usu
-INNER JOIN historico_reps AS his
+FROM SpotifyClone.usuarios AS usu
+INNER JOIN SpotifyClone.historico_reps AS his
 ON usu.usuario_id = his.usuario_id
-INNER JOIN cancoes AS can
+INNER JOIN SpotifyClone.cancoes AS can
 ON his.cancao_id = can.cancao_id
 GROUP BY usu.usuario;
