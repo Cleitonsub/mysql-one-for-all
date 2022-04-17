ae4144766a9f85be2e9463ac13e5617ec567c36f
Cleiton <cleitonrmacedo@gmail.com>
2022-04-17 16:29:30 -0300

    feat: Requisito 2 - Criado uma QUERY que exibe tres colunas

---
+++ desafio2.sql
@@ -0,0 +1,2 @@
+SELECT COUNT(DISTINCT ca.cancao_id) AS cancoes, COUNT(DISTINCT ar.artista_id) AS artistas, COUNT(DISTINCT al.album_id) AS albuns
+FROM SpotifyClone.cancoes AS ca, SpotifyClone.artistas AS ar, SpotifyClone.albuns AS al;
