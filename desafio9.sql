f9b67ee7ff9f4c47ff000b5fb527fdb2c648bb28
Cleiton <cleitonrmacedo@gmail.com>
2022-04-19 07:16:22 -0300

    fix: Requisito 9 - Alterado o nome da coluna para 'quantidade_musicas_no_historico'

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(his.usuario_id)
+SELECT COUNT(his.usuario_id) AS quantidade_musicas_no_historico
 FROM SpotifyClone.historico_reps AS his
 INNER JOIN SpotifyClone.usuarios AS usu
 ON his.usuario_id = usu.usuario_id
